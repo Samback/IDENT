3c01051f786555a87ac43192fa8226656f904fd6
Max Tymchii <Samback@ukr.net>
2012-01-20 23:12:30 +0200

    add exception class

---
+++ src/IDENTException.java
@@ -0,0 +1,14 @@
+
+public class IDENTException extends Throwable {
+	public IDENTException() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	public IDENTException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+		System.out.println("Exception happend " + message);
+		
+	}
+
+}
