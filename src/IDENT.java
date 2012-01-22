fbe00f98a03edf128dc57c961ea31874f039a0d8
Max Tymchii <Samback@ukr.net>
2012-01-22 21:13:50 +0200

    change number of input values

--- src/IDENT.java
+++ src/IDENT.java
@@ -2,7 +2,7 @@ public class IDENT {
 
 	public static void  main(String arg []) throws IDENTException {
 		try{
-			Calculation calc = new Calculation("1.txt", 3);
+			Calculation calc = new Calculation("1.txt", 6, 5);
 		}catch (Exception e) {
 			// TODO: handle exception
 			throw new IDENTException ();
