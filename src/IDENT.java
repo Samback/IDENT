f5bc573574fcf43dbf39968d6f2cbfef66541ac1
Max Tymchii <Samback@ukr.net>
2012-02-03 00:09:48 +0200

    try new functionality of java free charts

--- src/IDENT.java
+++ src/IDENT.java
@@ -1,13 +1,20 @@
+
+
+
 public class IDENT {
 
 	public static void  main(String arg []) throws IDENTException {
-		try{
-			Calculation calc = new Calculation("1.txt", 9, 8);
-		}catch (Exception e) {
-			// TODO: handle exception
-			throw new IDENTException ();
-		}
+//		try{
+//			Calculation calc = new Calculation("1.txt", 9, 8);
+//		}catch (Exception e) {
+//			// TODO: handle exception
+//			throw new IDENTException ();
+//		}
+		PieChart demo = new PieChart("Comparison", "Which operating system are you using?");
+        demo.pack();
+        demo.setVisible(true);
 		
 	}
 
 }
+
